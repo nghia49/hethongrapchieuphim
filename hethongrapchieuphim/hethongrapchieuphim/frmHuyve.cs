72c661cbf319aea9d1cb7da71226e887b37c8f79
Nghia49 <nghia040904@gmail.com>
2025-04-09 17:54:08 +0700

    Hien thi lai giao dien lich su dat ve va huy ve

--- hethongrapchieuphim/hethongrapchieuphim/frmHuyve.cs
+++ hethongrapchieuphim/hethongrapchieuphim/frmHuyve.cs
@@ -22,5 +22,6 @@ namespace hethongrapchieuphim
             //Gửi yêu cầu hủy vé và phản hồi
         }
         //Viết hàm hủy vé kiểm tra điều kiện và xóa vé trong csdl
+        //Hiển thị lại lịch sử đặt vé
     }
 }
