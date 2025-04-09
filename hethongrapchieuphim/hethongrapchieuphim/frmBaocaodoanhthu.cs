772d97204607300194b2d714933a155bc0f60ddc
Nghia49 <nghia040904@gmail.com>
2025-04-09 18:47:18 +0700

    Xu ly nut thong ke doanh thu

--- hethongrapchieuphim/hethongrapchieuphim/frmBaocaodoanhthu.cs
+++ hethongrapchieuphim/hethongrapchieuphim/frmBaocaodoanhthu.cs
@@ -16,5 +16,11 @@ namespace hethongrapchieuphim
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Thống kê doanh thu bán vé
+
+        }
     }
 }
