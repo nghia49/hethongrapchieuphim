9b97e1ea57a174529c0785c6b9f06077cb8589f6
Nghia49 <nghia040904@gmail.com>
2025-04-09 16:43:36 +0700

    Viet ham lay thong tin phim tu csdl

--- hethongrapchieuphim/hethongrapchieuphim/frmThongtinphim.cs
+++ hethongrapchieuphim/hethongrapchieuphim/frmThongtinphim.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace hethongrapchieuphim
 
         }
         //private void frmChiTietPhim_Load(object sender, EventArgs e)
+        //public static Phim LayChiTietPhim(string maPhim)
 
     }
 }
