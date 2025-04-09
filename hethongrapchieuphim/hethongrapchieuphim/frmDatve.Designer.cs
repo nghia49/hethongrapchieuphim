94927151b38ed5ab4c46322bf838b3bac267e6a4
Nghia49 <nghia040904@gmail.com>
2025-04-09 17:34:11 +0700

    Gui yeu cua dat ve

--- hethongrapchieuphim/hethongrapchieuphim/frmDatve.Designer.cs
+++ hethongrapchieuphim/hethongrapchieuphim/frmDatve.Designer.cs
@@ -54,8 +54,9 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
+            this.button1.Text = "Đặt";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // frmDatve
             // 
--- hethongrapchieuphim/hethongrapchieuphim/frmDatve.cs
+++ hethongrapchieuphim/hethongrapchieuphim/frmDatve.cs
@@ -16,5 +16,11 @@ namespace hethongrapchieuphim
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        //foreach (var ghe in danhSachGheDaChon)
     }
 }
