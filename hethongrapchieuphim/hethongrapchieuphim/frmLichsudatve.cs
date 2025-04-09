2ecb56ce64202c72f82d9c83d644c1c1e5880ce9
Nghia49 <nghia040904@gmail.com>
2025-04-09 17:41:26 +0700

    Tao giao dien Lich su dat ve

---
+++ hethongrapchieuphim/hethongrapchieuphim/frmLichsudatve.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hethongrapchieuphim
+{
+    public partial class frmLichsudatve : Form
+    {
+        public frmLichsudatve()
+        {
+            InitializeComponent();
+        }
+
+        private void frmLichsudatve_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
