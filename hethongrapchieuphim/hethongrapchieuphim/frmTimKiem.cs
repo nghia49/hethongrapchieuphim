8538fa756455bc95e0773821c5d6bbb78aa2f630
Nghia49 <nghia040904@gmail.com>
2025-04-09 16:27:59 +0700

    Xu li su kien Tim kiem

--- hethongrapchieuphim/hethongrapchieuphim/frmTimKiem.cs
+++ hethongrapchieuphim/hethongrapchieuphim/frmTimKiem.cs
@@ -21,5 +21,10 @@ namespace hethongrapchieuphim
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Xử lý sự kiện tìm kiếm
+        }
     }
 }
