47336d2ad7fab903a7485ac3fc628bc1bcf077cf
Nghia49 <nghia040904@gmail.com>
2025-04-10 23:07:59 +0700

    Xu ly nut xem trailer bang url da  luu trong csdl

--- hethongrapchieuphim/hethongrapchieuphim/frmXemtrailer.cs
+++ hethongrapchieuphim/hethongrapchieuphim/frmXemtrailer.cs
@@ -16,5 +16,10 @@ namespace hethongrapchieuphim
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Xử lý nút xem trailer bằng URL đã lưu trong cơ sở dữ liệu
+        }
     }
 }
