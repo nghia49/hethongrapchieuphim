52e58539fe9d4d7a89fbbe4b3fb1ca3ac26b4cc9
Nghia49 <nghia040904@gmail.com>
2025-04-10 22:50:42 +0700

    Xu ly nut cam nguoi dung va cap nhat vao csdl

--- hethongrapchieuphim/hethongrapchieuphim/frmQuanlynguoidung.cs
+++ hethongrapchieuphim/hethongrapchieuphim/frmQuanlynguoidung.cs
@@ -21,5 +21,10 @@ namespace hethongrapchieuphim
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Xử lý nút cấm người dùng và cập nhật trong csdl
+        }
     }
 }
